032ab706e7764069ea93f19e6a3602068244fcb2
9shhh <uelj1234@naver.com>
2023-02-12 16:26:09 +0900

    객체를 디비에 맞춘 설계 방식에서 데이터 영속화 및 조회 테스트

--- src/main/java/jpabook/Main.java
+++ src/main/java/jpabook/Main.java
@@ -12,39 +12,64 @@ import java.time.LocalDateTime;
 
 public class Main {
     public static void main(String[] args) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello"); // 애플리케이션 로딩 시점에 딱 1개만 생성되어야 함.
-
-        EntityManager em = emf.createEntityManager(); // 트랜잭션 단위 마다 생성 되어야함. 웹 기준, 고객의 요청이 올떄 마다 사용되고 close() 됨.
-
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
 
         tx.begin();
 
         try {
-            System.out.println("아니 왜 안돼");
             Member member = new Member();
-            member.setId(1L);
             member.setName("park");
 
             em.persist(member);
 
             Order order = new Order();
-            order.setId(1L);
             order.setMemberId(member.getId());
             order.setOrderDate(LocalDateTime.now());
             order.setOrderStatus(OrderStatus.ORDER);
 
             em.persist(order);
 
-            Order findOrder = em.find(Order.class, 1L);
+            System.out.println(member.getId());
+            System.out.println(order.getId());
+
+            Member member1 = em.find(Member.class, member.getId());
+            Order order1 = em.find(Order.class, order.getId());
+
+            System.out.println(member1);
+            System.out.println(order1);
+
+            em.flush();
+            System.out.println("==========flush==========");
+            em.clear();
+            System.out.println("==========clear==========");
+
+            System.out.println(member.getId());
+            System.out.println(order.getId());
+
+            Order findOrder = em.find(Order.class, order.getId());
+
+            System.out.println(findOrder);
+
             Long memberId = findOrder.getMemberId(); // 회원 식별자를 조회
 
+            System.out.println(memberId);
+
             Member findMember = em.find(Member.class, memberId);// 객체 그래프 탐색 방식이 아닌 order 객체에 저장된 회원 식별자로 맴버를 조회함 -> bad.
+
+            System.out.println(findMember);
             System.out.println(findMember.getId());
             System.out.println(findMember.getName());
 
+            // 영속성화 되어 있는지 확인
+            em.find(Order.class, order.getId());
+            em.find(Member.class, memberId);
+            System.out.println("==========select query run?==========");
+
             tx.commit();
         } catch (Exception e) {
+            e.printStackTrace();
             tx.rollback();
         } finally {
             em.close();
